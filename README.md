f02b24274e6ee2550e971817b8612644a283e696
weebagency <cedricm@weebagency.com>
2018-02-05 12:48:05 -0500

    dockerizing the boilerplate

--- README.md
+++ README.md
@@ -3,4 +3,8 @@
 
 ## Run
 
+### Simple test
 > php -S localhost:8080 index.php
+
+### With Docker
+> docker-compose up --build
--- index.php
+++ index.php
@@ -4,6 +4,12 @@ $loader = require __DIR__ . '/vendor/autoload.php';
 
 use TestApp\App;
 
+// Test DB
+/*
+mysqli_connect("db", "root", "test") or die(mysqli_error());
+echo "Connected to MySQL<br />";
+*/
+
 App::Bootstrap();
 
 
